824e9e07a259066bd6bc6678a6fc51e98f3e6c5f
adan llanos <adanllanos96@gmail.com>
2023-11-16 18:42:01 -0400

    recuperar informacion de un alojamiento

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -13,36 +13,17 @@ use Illuminate\Support\Facades\DB;
 
 class FilterController extends Controller
 {
-    /* public function getAllPropertieswhitCity($city, $hosts)
-    {
-        DB::statement("SET SQL_MODE=''");
-
-        $properties = Properties::select('idProperty', 'propertyName', 'propertyAmount', 'propertyAbility', 'images.imageLink', 'images.property_id', 'propertydescription', 'propertyCity', 'propertyStatus')
-            ->join(DB::raw('(SELECT * FROM images GROUP BY property_id) as images'), function ($join) {
-                $join->on('properties.idProperty', '=', 'images.property_id');
-            })
-            ->where('propertycity', 'LIKE', '%' . $city . '%')
-            ->where('propertyStatus', 'disponible')
-            ->where('propertyAbility', $hosts)
-            //->whereBetween('fecha', ['2022-01-01', '2022-01-31'])
-            ->get();
-
-        return response()->json($properties);
-    } */
     public function getAllPropertiesWithCity(Request $request)
     {
 
         DB::statement("SET SQL_MODE=''");
         $city = $request->input('city');
         $hosts = $request->input('hosts');
-        // Verificar si tanto $city como $hosts están vacíos
         if (empty($city) && empty($hosts)) {
             return response()->json(['error' => 'City and hosts cannot be empty']);
         }
 
-        // Verificar si solo $city está vacío
         if (empty($city)) {
-            // Si $city está vacío pero $hosts tiene valor, filtrar solo por $hosts
             $properties = Properties::select('idProperty', 'propertyName', 'propertyAmount', 'propertyAbility', 'images.imageLink', 'images.property_id', 'propertydescription', 'propertyCity', 'propertyStatus')
                 ->join(DB::raw('(SELECT * FROM images GROUP BY property_id) as images'), function ($join) {
                     $join->on('properties.idProperty', '=', 'images.property_id');
@@ -55,9 +36,7 @@ class FilterController extends Controller
             return response()->json($properties);
         }
 
-        // Verificar si solo $hosts está vacío
         if (empty($hosts)) {
-            // Obtener todas las propiedades sin considerar hosts
             $properties = Properties::select('idProperty', 'propertyName', 'propertyAmount', 'propertyAbility', 'images.imageLink', 'images.property_id', 'propertydescription', 'propertyCity', 'propertyStatus')
                 ->join(DB::raw('(SELECT * FROM images GROUP BY property_id) as images'), function ($join) {
                     $join->on('properties.idProperty', '=', 'images.property_id');
@@ -70,7 +49,6 @@ class FilterController extends Controller
             return response()->json($properties);
         }
 
-        // Si ambos $city y $hosts tienen valores, filtrar por ambas condiciones
         $properties = Properties::select('idProperty', 'propertyName', 'propertyAmount', 'propertyAbility', 'images.imageLink', 'images.property_id', 'propertydescription', 'propertyCity', 'propertyStatus')
             ->join(DB::raw('(SELECT * FROM images GROUP BY property_id) as images'), function ($join) {
                 $join->on('properties.idProperty', '=', 'images.property_id');
